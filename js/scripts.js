ebc21234e982bd0751486c209a329afe0724ec31
HoldenJC <holdenj_c@yahoo.com>
2019-06-14 10:02:33 -0700

    add functionality to take user input and return range from 0 up to and including user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 var userNum;
 
+
+
+
+var range = function(input){
+
+  for (var i = 0; i <= input; i++){
+    $("#result").append(i);
+
+  }
+
+}
+
+
 $(function(){
   $("#initial-number").submit(function(event){
     event.preventDefault();
     // resetFormField = "";
+    $("#result").empty();
 
     userNum = parseInt($("#initialNumber").val());
 
+    range(userNum);
 
-
-    $("#result").empty().append(userNum);
+    // $("#result").empty().append(userNum);
   });
 });
