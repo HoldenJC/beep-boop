3d2a5f1ea2aef5b2413a715e2e841a352c581321
HoldenJC <holdenj_c@yahoo.com>
2019-06-14 10:20:51 -0700

    add functionality to handle any number following rules outlined in README.md file

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,31 @@
-var userNum;
-
-
-
+var userNum, checkNum;
 
 var range = function(input){
-
   for (var i = 0; i <= input; i++){
-    $("#result").append(i);
-
+    checkNum = i.toString();
+    if (checkNum.match(/[3]/gi)){
+      $("#result").append("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (checkNum.match(/[2]/gi)){
+      $("#result").append("Boop!");
+    } else if (checkNum.match(/[1]/gi)){
+      $("#result").append("Beep!");
+    } else {
+      $("#result").append(i);
+    }
+    if (i < input){
+      $("#result").append(", ");
+    }
   }
-
 }
-
 
 $(function(){
   $("#initial-number").submit(function(event){
     event.preventDefault();
-    // resetFormField = "";
     $("#result").empty();
 
     userNum = parseInt($("#initialNumber").val());
 
     range(userNum);
 
-    // $("#result").empty().append(userNum);
   });
 });
